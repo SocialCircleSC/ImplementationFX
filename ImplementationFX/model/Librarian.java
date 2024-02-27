e932a38e2072de708b7763d9d5edcb0fdee1777d
vnyabrown <vnyabrown@gmail.com>
2024-02-27 13:33:06 -0500

    Update Librarian with initial view functionalities

--- ImplementationFX/model/Librarian.java
+++ ImplementationFX/model/Librarian.java
@@ -2,12 +2,102 @@
 package model;
 
 // system imports
+import java.util.Hashtable;
 import java.util.Properties;
+
+import event.Event;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 // project imports
 import impresario.IModel;
 import impresario.IView;
+import impresario.ModelRegistry;
+import userinterface.MainStageContainer;
+
+import event.Event;
+import userinterface.View;
+import userinterface.ViewFactory;
+import userinterface.WindowPosition;
 
 public class Librarian implements IView, IModel {
-    
-}
+// This class implements all these interfaces (and does NOT extend 'EntityBase')
+// because it does NOT play the role of accessing the back-end database tables.
+// It only plays a front-end role. 'EntityBase' objects play both roles.
+
+    // For Impresario
+    private Properties dependencies;
+    private ModelRegistry myRegistry;
+
+    // Instead of AccountHolder, we will use instances of our Book and Patron
+    private Book myBook;
+    private Patron myPatron;
+
+    // GUI Components
+    private Stage myStage;
+    private Hashtable<String, Scene> myViews;
+
+    public Librarian()
+    {
+        myStage = MainStageContainer.getInstance(); // Get the main stage to be used to change/update scenes
+        myViews = new Hashtable<String, Scene>(); // 
+
+        // STEP 3.1: Create the Registry object - if you inherit from
+        myRegistry = new ModelRegistry("Librarian");
+        if(myRegistry == null)
+        {
+            new Event(Event.getLeafLevelClassName(this), "Librarian",
+				"Could not instantiate Registry", Event.ERROR);
+        }
+
+        // STEP 3.2: Be sure to set the dependencies correctly
+        setDependencies(); // Need to create this function below
+
+        // Set up the initial view
+		createAndShowLibrarianView(); // Need to create this function below
+
+    } // end of Librarian constructor
+
+    //-----------------------------------------------------------------------------------
+    private void setDependencies()
+    {
+        
+    } // end of setDependencies
+
+    // Create Initial Startup View of program to be displayed
+    //-----------------------------------------------------------------------------------
+    private void createAndShowLibrarianView()
+    {
+        Scene currentScene = (Scene)myViews.get("LibrarianView");
+
+        if (currentScene == null)
+		{
+			// create our initial view
+			View newView = ViewFactory.createView("LibrarianView", this); // USE VIEW FACTORY
+			currentScene = new Scene(newView);
+			myViews.put("LibrarianView", currentScene);
+		}
+				
+		swapToView(currentScene); // Need to create this function below
+    }
+
+    public void swapToView(Scene newScene)
+    {
+        if (newScene == null) // Make sure that we have a new view/scene to swap to
+        {
+            System.out.println("Librarian.swapToView(): Missing view for display");
+			new Event(Event.getLeafLevelClassName(this), "swapToView",
+				"Missing view for display ", Event.ERROR);
+			return;
+        }
+
+        myStage.setScene(newScene);
+        myStage.sizeToScene();
+
+        // Place screen in center
+        WindowPosition.placeCenter(myStage);
+    }
+
+} // ENd of Librarian Class
+
+
