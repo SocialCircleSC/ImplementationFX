d650dd0c3f4eebc38b2fb05235a787dd2d3e0863
vnyabrown <vnyabrown@gmail.com>
2024-02-27 13:04:03 -0500

    Created Librarian class

---
+++ ImplementationFX/model/Librarian.java
@@ -0,0 +1,13 @@
+// specify the package
+package model;
+
+// system imports
+import java.util.Properties;
+
+// project imports
+import impresario.IModel;
+import impresario.IView;
+
+public class Librarian implements IView, IModel {
+    
+}
